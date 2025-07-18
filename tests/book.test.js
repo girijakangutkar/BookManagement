72c49ef1356c2131c98dae574e62e4725d5d2292
Girija-Kangutkar <kangutkargirija075@gmail.com>
2025-07-18 18:03:42 +0530

    yml

--- tests/book.test.js
+++ tests/book.test.js
@@ -8,11 +8,11 @@ let userId;
 let bookId;
 
 beforeAll(async () => {
-  if (!process.env.Mongo_URI) {
+  if (!process.env.MONGO_URI) {
     throw new Error("Mongo URI is not defined");
   }
 
-  await mongoose.connect(process.env.Mongo_URI, {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     family: 4,
